2d1eebaa54d915d6266d389bea50b6b1f6da9f80
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2023-08-14 11:06:01 -0300

    Create Program.cs

---
+++ Troco/Troco/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            float celsius, fahrenheit;
+
+
+            Console.WriteLine("digite a temperatura em celsius: ");
+            celsius = float.Parse(Console.ReadLine());
+
+            fahrenheit = celsius * 9 / 5 + 32;
+
+            Console.WriteLine("a temperatura em fahrenheit é: " + fahrenheit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
